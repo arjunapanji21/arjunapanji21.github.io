8dbac6ac81055237868c18936b4a65f87ff825c9
arjunapanji21 <arjuna.panji97@gmail.com>
2021-04-14 00:57:32 +0700

    initial commit

---
+++ theme-color.js
@@ -0,0 +1,29 @@
+let theme = localStorage.getItem('theme')
+
+if (theme == null){
+    setTheme('light')
+}
+else{
+    setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+for (var i=0; themeDots.length > i; i++){
+    themeDots[i].addEventListener('click', function(){
+        let mode = this.dataset.mode
+        console.log('You are in', mode, 'theme.')
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode){
+    if(mode == 'light'){
+        document.getElementById('theme-style').href = 'default.css'
+    }
+    if(mode == 'dark'){
+        document.getElementById('theme-style').href = 'dark.css'
+    }
+
+    localStorage.setItem('theme', mode)
+}
