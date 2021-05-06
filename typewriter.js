7b3c65f3a94f44c0df3cbabcc8c0ec110d5f027d
Arjuna Panji <75791180+arjunapanji21@users.noreply.github.com>
2021-05-07 06:36:47 +0700

    Update typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -1,6 +1,6 @@
 var i = 0;
 var txt = 'I can do everything about programming because i love to, i can fix your network, and even fix your printer too :D'
-var speed = 100; /* The speed/duration of the effect in milliseconds */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
   if (i < txt.length) {
